a9bbcef968ec1b8c08910a818c21e30edf2f80b7
Fares Gamal <ferasjamal55@gmail.com>
2022-03-20 18:15:13 +0200

    Static File
    Applied Static files by:
    1. Declaring static variable STATIC_DIR
    2. Declaring Static dirs list
    3. Referring to css style and image using static reference

--- Django_One/first_project/first_project/settings.py
+++ Django_One/first_project/first_project/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -118,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
