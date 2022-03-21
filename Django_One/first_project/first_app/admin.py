0bfb421c38e2004295f08155d2280426fbae12f5
Fares Gamal <ferasjamal55@gmail.com>
2022-03-21 14:37:00 +0200

    Creating Models
    Created three database models (tables)
    Registered a superuser admin
    Registered the models into the admin site

--- Django_One/first_project/first_app/admin.py
+++ Django_One/first_project/first_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from first_app.models import Topic, Webpage, AccessRecord
 
 # Register your models here.
+admin.site.register(Topic)
+admin.site.register(AccessRecord)
+admin.site.register(Webpage)
