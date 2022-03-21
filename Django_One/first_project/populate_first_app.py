cea5a8a795a7a4255ab2524755de30665c4dfcba
Fares Gamal <ferasjamal55@gmail.com>
2022-03-21 15:35:16 +0200

    Populating Script
    Added Faker package
    Added on environment setup
    Created list of fake topic names
    Created topic
    Populated webpages and access records using faked attributes

---
+++ Django_One/first_project/populate_first_app.py
@@ -0,0 +1,43 @@
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_project.settings')
+import django
+
+django.setup()
+
+# FAKE POP SCRIPT
+import random
+from first_app.models import AccessRecord, Webpage, Topic
+from faker import Faker
+
+fakegen = Faker()
+topics = ['Search', 'Social', 'Market', 'News', 'Games']
+
+
+def add_topic():
+    t = Topic.objects.get_or_create(top_name=random.choice(topics))[0]
+    t.save()
+    return t
+
+
+def populate(n=5):
+    for entry in range(n):
+        # get the topic for entry
+        top = add_topic()
+
+        # create the fake data for that entry
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
+
+        # Create the new webpage entry
+        webpg = Webpage.objects.get_or_create(topic=top, url=fake_url, name=fake_name)[0]
+
+        # create access record for that page
+        acc_rec = AccessRecord.objects.get_or_create(name=webpg, date=fake_date)[0]
+
+
+if __name__ == '__main__':
+    print("Populating Scripts")
+    populate(20)
+    print("Database has been populated")
